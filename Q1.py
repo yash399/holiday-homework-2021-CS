b74ed375cff8e2236d1695681be12293ed8b8bf5
yash399 <85052719+yash399@users.noreply.github.com>
2021-05-30 12:36:02 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,10 @@
+#write a program to enter two integers and perform all arithmetic operations on them.
+int1=int(input("enter first integer"))
+int2=int(input("enter second integer"))
+add=int1+int2
+sub=int1-int2
+divide=int1/int2
+multiply=int1*int2
+print(f"addition={add}, subtraction={sub}, division={divide}, multiplication={multiply}")
+
+
---
+++ Q2.py
@@ -0,0 +1,7 @@
+#write a program to find the average of three numbers.
+#variables(a,b,c)=list(map(int,input("enter the values of a,b,c").split()))
+a=float(input("enter first number"))
+b=float(input("enter second number"))
+c=float(input("enter third number"))
+average=(a+b+c)/3
+print(average)
