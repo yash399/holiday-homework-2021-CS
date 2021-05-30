81e6b3386485ceea5c94b0c89b1758436806cfc3
yash399 <85052719+yash399@users.noreply.github.com>
2021-05-30 13:03:35 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,7 @@
+#write a program to swap two numbers without using a third variable.
+int1=int(input("enter first number"))
+int2=int(input("enter second number"))
+int1,int2=int2,int1
+
+print(int1,int2)
+
