2c64ed8f8a6910c345f33c55e003218f062eead8
yash399 <85052719+yash399@users.noreply.github.com>
2021-05-30 12:59:29 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,8 @@
+#write a program to swap two numbers using a third variable.
+int1=int(input("enter first number"))
+int2=int(input("enter second number"))
+a=int1
+int1=int2
+int2=a
+print(int1,int2)
+
