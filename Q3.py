248b4634147d166e0a55d7330544e555db8b2032
yash399 <85052719+yash399@users.noreply.github.com>
2021-05-30 12:48:44 +0530

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,3 @@
+#write a program to repeat the string "GOOD MORNING" n number of times. here "n" is a integer entered by the user.
+n=int(input("enter the number of times you want to print GOOD MORNING"))
+print("GOOD MORNING \n"*n)
