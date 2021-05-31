4887c0727129266d61375b2ff60b908cde32ad23
yash399 <85052719+yash399@users.noreply.github.com>
2021-05-31 08:15:08 +0530

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,4 @@
+#write a program that asks the user to enter their name and age.print a message addressed to the user that tells the user the year in which they will turn 100 years.
+name = input("enter your name")
+age = int(input("enter your age"))
+print("year when you will turn 100 is",100-age+2021)
