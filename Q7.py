f96f7503371ccaa16c0fa4853fd533ce6ef6240f
yash399 <85052719+yash399@users.noreply.github.com>
2021-05-31 08:06:11 +0530

    Add files via upload

--- Q7.py
+++ Q7.py
@@ -1,1 +1,4 @@
 #program to convert temprature in degree in degree celsius to degree fahrenheit.
+celsius = int(input("enter the temprature in degrees celsius that you would like to convert"))
+fahrenheit = (celsius*9/5) + 32
+print("the converted temprature is", fahrenheit,"degrees fahrenheit")
